7d37686f0164724743c6c9ee40159342c3af5a75
Mr.Wolf <ufon2009@gmail.com>
2018-11-21 17:17:58 +0100

    Initial commit

---
+++ Biologie_křížovka/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace pokusTextFile
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var pokracovat = true;
+            var moznaSlova = new List<string>();
+            var moznaSlovaPisemno = new List<string>();
+
+            var logFile = File.ReadAllText(@"C:\Users\ufon2\OneDrive\Dokumenty\biologie.txt", Encoding.Default);
+
+            var punctuation = logFile.Where(Char.IsPunctuation).Distinct().ToArray();
+            var words = logFile.Split().Select(x => x.Trim(punctuation));
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine("Zadejte kolik písmen má slovo obsahovat");
+            var delkaSlova = Convert.ToInt32(Console.ReadLine());
+
+            foreach (var slovo in words)
+            {
+                if (slovo.Length == delkaSlova && !slovo.Contains("ch"))
+                {
+                    moznaSlova.Add(slovo);
+                }
+
+                if (slovo.Contains("ch"))
+                {
+                    if (slovo.Length - 1 == delkaSlova)
+                    {
+                        moznaSlova.Add(slovo);
+                    }
+                }
+            }
+
+            foreach (var slovo in moznaSlova)
+            {
+                moznaSlovaPisemno.Add(slovo);
+            }
+
+            foreach (var slovo in moznaSlova)
+            {
+                Console.WriteLine(slovo);
+            }
+
+            while (pokracovat)
+            {
+                Console.WriteLine("Zadejte písmeno které má slovo obsahovat");
+
+
+                var pismeno = Console.ReadLine().ToLower();
+
+
+                if (pismeno.Length != 1 && pismeno != "ch")
+                    pismeno = pismeno.Substring(0, 1);
+                bool obsahujePismeno;
+
+                foreach (var slovo in moznaSlova)
+                {
+                    obsahujePismeno = false;
+
+                    if (slovo.ToLower().Contains(pismeno))
+                    {
+                        obsahujePismeno = true;
+                    }
+
+                    if (!obsahujePismeno)
+                    {
+                        moznaSlovaPisemno.Remove(slovo);
+                    }
+                }
+
+                if (moznaSlovaPisemno.Count == 0)
+                {
+                    Console.WriteLine("Žádné slovo nesplňujě podmínky");
+                }
+                else
+                {
+                    Console.WriteLine("Slova po vyfiltrovaní");
+                    foreach (var slovo in moznaSlovaPisemno)
+                    {
+                        Console.WriteLine(slovo);
+                    }
+
+                    Console.WriteLine("Chcete zadat další písmeno ? A/N");
+                    if (Console.ReadLine() != "a")
+                    {
+                        pokracovat = false;
+                    }
+                }
+            }
+        }
+    }
+}
